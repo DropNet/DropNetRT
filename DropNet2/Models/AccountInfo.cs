f0c7331cfef022dba1e7159debc66806aa1b0a33
Prashant Cholachagudda <pvc@outlook.com>
2013-03-28 22:55:11 +0530

    Renamed the AccountInfo properties to upper camel case

--- DropNet2/Models/AccountInfo.cs
+++ DropNet2/Models/AccountInfo.cs
@@ -1,19 +1,37 @@
-﻿namespace DropNet2.Models
+﻿using System.Runtime.Serialization;
+
+namespace DropNet2.Models
 {
     public class AccountInfo
     {
-        public string referral_link { get; set; }
-        public string country { get; set; }
-        public string email { get; set; }
-        public string display_name { get; set; }
-        public QuotaInfo quota_info { get; set; }
-        public long uid { get; set; }
+        [DataMember(Name = "referral_link")]
+        public string ReferralLink { get; set; }
+
+        [DataMember(Name = "country")]
+        public string Country { get; set; }
+
+        [DataMember(Name ="email")]
+        public string Email { get; set; }
+
+        [DataMember(Name ="display_name")]
+        public string DisplayName { get; set; }
+
+        [DataMember(Name = "quota_info")]
+        public QuotaInfo QuotaInfo { get; set; }
+
+        [DataMember(Name = "uid")]
+        public long Uid { get; set; }
     }
 
     public class QuotaInfo
     {
-        public long shared { get; set; }
-        public long quota { get; set; }
-        public long normal { get; set; }
+        [DataMember(Name = "shared")]
+        public long Shared { get; set; }
+
+        [DataMember(Name = "quota")]
+        public long Quota { get; set; }
+        
+        [DataMember(Name = "normal")]
+        public long Normal { get; set; }
     }
 }
